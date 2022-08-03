047362459f4cd52f778d37ec0a090cc305b58fff
wangyizhu <wyzarsenal@gmail.com>
2022-08-03 16:52:41 +0800

    first commit

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- 组件结构 -->
+    <div class="demo">
+        <h2>学校名称：{{schoolName}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+    //组件交互
+    export default{
+        name:'School',
+        data(){
+            schoolName:'gwu',
+            address:'dc'
+        },
+        methods:{
+            showName(){
+                alter(this.schoolName)
+            }
+        }
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color:orange
+    }
+</style>
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- 组件结构 -->
+    <div class="demo">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    //组件交互
+    export default{
+        name:'Student',
+        data(){
+            name:'lk',
+            age:18
+        }
+    }
+</script>
